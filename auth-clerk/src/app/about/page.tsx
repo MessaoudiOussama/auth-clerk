9537a502f7952ad8a96c9f1a964ec9aea1cee723
MessaoudiOussama <92897172+MessaoudiOussama@users.noreply.github.com>
2024-09-09 18:19:08 +0000

    first

---
+++ auth-clerk/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="font-blond text-center mt-10">About Page</h1>
+    </div>
+  );
+}
---
+++ auth-clerk/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-blond text-center mt-10">Home Page</h1>
+    </div>
+  );
+}
